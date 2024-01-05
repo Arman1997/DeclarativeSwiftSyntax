633166ba8952ec4b6bcaddf887343a6786251ac6
Arman Galstyan <armangalstyan9597@gmail.com>
2024-01-05 16:57:32 +0400

    Add dependencies

--- DeclarativeSwiftSyntax/Package.swift
+++ DeclarativeSwiftSyntax/Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "DeclarativeSwiftSyntax",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DeclarativeSwiftSyntax",
             targets: ["DeclarativeSwiftSyntax"]),
     ],
+    dependencies: [
+      .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.2"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DeclarativeSwiftSyntax"),
+            name: "DeclarativeSwiftSyntax",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ]
+        ),
         .testTarget(
             name: "DeclarativeSwiftSyntaxTests",
             dependencies: ["DeclarativeSwiftSyntax"]),
